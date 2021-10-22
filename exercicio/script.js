58f463d3e5a687ddea36c29c413f0f8b2a57b66a
priKSouza <priscilakarinajump@gmail.com>
2021-10-22 15:32:53 -0300

    Entregando o exercício da semana :dancers:

--- exercicio/script.js
+++ exercicio/script.js
@@ -1,26 +1,81 @@
 const json = [
   {
     "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
+    "foto": "../assets/lini.jpg",
     "generos": ["MPB", "Soul", "R&B", "Blues"],
     "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
     "instagram": "https://www.instagram.com/linikeroficial/"
   },
   {
-    "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
-    "generos": ["MPB", "Soul", "R&B", "Blues"],
-    "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
-    "instagram": "https://www.instagram.com/linikeroficial/"
+    "nome": "Doralyce",
+    "foto": "../assets/dora.jpg",
+    "generos": ["Pop"],
+    "discografia": ["Festa Boa", "Miss Beleza Universal", "Para de apontar o dedo"], 
+    "instagram": "https://www.instagram.com/missbelezauniversal/"
   },
   {
-    "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
-    "generos": ["MPB", "Soul", "R&B", "Blues"],
-    "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
-    "instagram": "https://www.instagram.com/linikeroficial/"
+    "nome": "Maria Bethânia",
+    "foto": "../assets/maria.jpg",
+    "generos": ["MPB", "Blues"],
+    "discografia": ["Carta de amor", "Fera Ferida", "Onde estará o meu amor?"], 
+    "instagram": "https://www.instagram.com/mariabethaniaoficial/"
   }
-]
+];
+
+
+const fotoLiniker = document.getElementById("foto");
+const tituloLiniker = document.getElementById("titulo-1");
+const generosLiniker = document.getElementById("genero");
+const discografiaLiniker = document.getElementById("discografia");
+const instagramLiniker = document.getElementById("instagram");
+
+function fichaLiniker() {
+  fotoLiniker.setAttribute('src', json[0].foto);
+  tituloLiniker.innerText = json[0].nome;
+  generosLiniker.innerText = json[0].generos.join(" - ");
+  discografiaLiniker.innerText = json[0].discografia.join(" - ");
+  instagramLiniker.setAttribute('href', json[0].instagram);
+}
+console.log(json[0].discografia);
+
+fichaLiniker();
+
+const fotoDoralyce = document.getElementById("foto-2");
+const tituloDoralyce = document.getElementById("titulo-2");
+const generosDoralyce = document.getElementById("genero-2");
+const discografiaDoralyce = document.getElementById("discografia-2");
+const instagramDoralyce = document.getElementById("instagram-2");
+
+function fichaDolaryce() {
+  fotoDoralyce.setAttribute('src', json[1].foto);
+  tituloDoralyce.innerText = json[1].nome;
+  generosDoralyce.innerText = json[1].generos.join(" - ");
+  discografiaDoralyce.innerText = json[1].discografia.join(" - ");
+  instagramDoralyce.setAttribute('href', json[1].instagram);
+}
+console.log(json[1].discografia);
+
+fichaDolaryce();
+
+const fotoMaria = document.getElementById("foto-3");
+const tituloMaria = document.getElementById("titulo-3");
+const generosMaria = document.getElementById("genero-3");
+const discografiaMaria = document.getElementById("discografia-3");
+const instagramMaria = document.getElementById("instagram-3");
+
+function fichaMaria() {
+  fotoMaria.setAttribute('src', json[2].foto);
+  tituloMaria.innerText = json[2].nome;
+  generosMaria.innerText = json[2].generos.join(" - ");
+  discografiaMaria.innerText = json[2].discografia.join(" - ");
+  instagramMaria.setAttribute('href', json[2].instagram);
+}
+console.log(json[2].discografia);
+
+fichaMaria();
+
+
+
 
 /* 
 
