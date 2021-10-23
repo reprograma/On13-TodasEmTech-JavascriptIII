d69f658f1efa6c70c074c3206ee06d2e5752154b
fabiula-rodrigues <mfabi463@gmail.com>
2021-10-22 23:26:49 -0300

    exercicio

---
+++ pratica/script.js
@@ -0,0 +1,9 @@
+const objetoJson = {
+  "nome": "Liniker",
+  "foto": "../assets/liniker.webp",
+  "generos": ["MPB", "Soul", "R&B", "Blues"],
+  "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
+  "instagram": "https://www.instagram.com/linikeroficial/"
+}
+
+// Suponha que nosso projeto consiste em um site de informações sobre cantoras brasileiras e na página em questão a usuária selecionou a cantora Liniker, e a API nos retornou um JSON com todas as informações que precisamos (armazenado na variável objetoJson acima), então devemos popular nosso HTML com essas informações utilizando JavaScript 
